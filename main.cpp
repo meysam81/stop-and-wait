352458bc1d9f527408a43fd6989029ecd50771d7
Meysam Azad <MeysamAzad81@yahoo.com>
2017-12-27 07:32:25 +0330

    main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
-#include <QCoreApplication>
+#include <iostream>
+using namespace std;
+#include <thread>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
 
-    return a.exec();
+    return 0;
 }
 
