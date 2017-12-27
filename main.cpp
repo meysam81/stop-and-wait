6e1ba34c34a89dec80ef8dd4bb23d44ac9a9005c
Meysam Azad <MeysamAzad81@yahoo.com>
2017-12-27 10:03:51 +0330

    a thread for each side

--- main.cpp
+++ main.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 #include <thread>
 
+// these 2 babies are going to be defined else where
+extern int client(char *argv[]);
+extern int server(char *argv[]);
+
 int main(int argc, char *argv[])
 {
+    if (argc != 3)
+    {
+        perror("Usage: <port> <error_rate>");
+        exit(1);
+    }
+//    thread t1(&client, argv);
+    thread t2(&server, argv);
+
+//    t1.join();
+    t2.join();
 
     return 0;
 }
