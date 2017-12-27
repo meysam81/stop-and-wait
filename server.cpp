163b780b87f03c67c04325de4c05bcca190ca3b4
Meysam Azad <MeysamAzad81@yahoo.com>
2017-12-27 10:03:22 +0330

    added artificial error occurrence

--- server.cpp
+++ server.cpp
@@ -13,49 +13,49 @@
 #include <thread>
 #include <chrono>
 using namespace std;
-extern void error(const char *msg);
-int server(int argc, char *argv[])
+extern void error(const char *msg); // extern: it is going to be defined else where
+int server(char *argv[])
 {
-    int sock, length, n;
-    socklen_t fromlen;
+    // ============================================== Initializing ==============================================
     struct sockaddr_in server;
+    int sock, length = sizeof(server), n;
+    socklen_t fromlen = sizeof(struct sockaddr_in);;
     struct sockaddr_in from;
     char buf[1024];
-
-    if (argc != 3) {
-        fprintf(stderr, "Usage: port error_rate");
-        exit(0);
-    }
-//    int error_rate = argv[2] * 10;
-    sock=socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) error("Opening socket");
-    length = sizeof(server);
-    bzero(&server,length);
-    server.sin_family=AF_INET;
-    server.sin_addr.s_addr=INADDR_ANY;
-    server.sin_port=htons(atoi(argv[1]));
-    if (bind(sock,(struct sockaddr *)&server,length)<0)
-        error("binding");
-    fromlen = sizeof(struct sockaddr_in);
     int i = 0;
+    int error_rate = atoi(argv[2]) * 10;
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0); // UDP socket for ipv4
+    if (sock < 0) error("Opening socket"); // failed initializing socket
+    bzero(&server,length); // fill the struct with zeroz
+    server.sin_family=AF_INET; // ipv4 address
+    server.sin_addr.s_addr=INADDR_ANY; // get packets from all ip's
+    server.sin_port=htons(atoi(argv[1])); // port
+    if (bind(sock,(struct sockaddr *)&server,length)<0) // bind to the indicated ip & port
+        error("binding");
+
     while (1) {
-//        if (i % 10 == error_rate)
-//        {
-//            i++;
-//            continue;
-//        }
-        n = recvfrom(sock,buf,1024,0,(struct sockaddr *)&from,&fromlen);
-        if (n < 0) error("recvfrom");
-        this_thread::sleep_for(chrono::milliseconds(10));
-//        string t = "Got your message " + i;
-        char * t = "Got your message";
-        n = sendto(sock, t, sizeof(t),
+        if (i % 10 == error_rate) // artificial packet loss
+        {
+            i++;
+            continue;
+        }
+        n = recvfrom(sock,buf,1024,0,(struct sockaddr *)&from,&fromlen); //receive from client
+        if (n < 0) continue; // wait for another packet i.e stop & wait
+        this_thread::sleep_for(chrono::milliseconds(10)); // process the packet for 10 0.1 second
+
+        // ack message
+        char * t = "Got your message: ";
+        char * temp1 = const_cast<char*>(to_string(i).c_str());
+
+
+        n = sendto(sock, t, sizeof(t), // send ack
                    0,(struct sockaddr *)&from,fromlen);
-        if (n  < 0)
+        if (n  < 0) // failed to send ack
             continue;
         write(1,"Received a datagram: ",21);
         write(1,buf,n);
-        i++;
+        i++; // wait for next message
     }
     return 0;
 }
